9c5c630017fd50cae7547cc25e7f84b082a9661f
ImRoodyDev <roodyalexis36@gmail.com>
2025-11-11 14:01:00 +0100

    fix: issue in inputfield

--- src/components/elements/FormInputField.tsx
+++ src/components/elements/FormInputField.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import {joinClsx} from "../../utils/classPrefixer";
-import clsx from "clsx";
+import { joinClsx } from '../../utils/classPrefixer';
+import clsx from 'clsx';
 
 type Props = {
 	label: string;
@@ -11,21 +11,14 @@ type Props = {
 } & Omit<React.InputHTMLAttributes<HTMLInputElement>, 'className'>;
 
 function FormInputField(props: Props) {
-	const {isError, label, error, mainClassName, className, ...inputProps} = props;
+	const { isError, label, error, mainClassName, className, ...inputProps } = props;
 	return (
 		<div className={clsx(mainClassName, className)}>
-			<label
-				htmlFor={inputProps.id}
-				className={clsx("input-field-label", joinClsx(className, 'input'))}
-			>{label}
+			<label htmlFor={inputProps.name} className={clsx('input-field-label', joinClsx(className, 'input'))}>
+				{label}
 			</label>
-			<input className={clsx("input-field")} {...inputProps}/>
-			{
-				isError && (
-					<div className={clsx("input-field-error-feedback", joinClsx(className, 'error-feedback'))}>
-						{error}
-					</div>
-				)}
+			<input className={clsx('input-field')} {...inputProps} />
+			{isError && <div className={clsx('input-field-error-feedback', joinClsx(className, 'error-feedback'))}>{error}</div>}
 		</div>
 	);
 }
